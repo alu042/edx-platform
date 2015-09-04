2bd4d9976971365c6d773f25733a8ae3e6c9902c
Bill DeRusha <bill@edx.org>
2015-09-04 15:36:19 -0400

    Add more defensive code around elasticsearch connection

--- lms/djangoapps/teams/errors.py
+++ lms/djangoapps/teams/errors.py
@@ -15,6 +15,9 @@ class AlreadyOnTeamInCourse(TeamAPIRequestError):
     """User is already a member of another team in the same course."""
     pass
 
+class ElasticSearchConnectionError(TeamAPIRequestError):
+    """System was unable to connect to the configured elasticsearch instance"""
+    pass
 
 class ImmutableMembershipFieldException(Exception):
     """An attempt was made to change an immutable field on a CourseTeamMembership model"""
